acfa324c7b11ee0de89745e843a0c9f6324c062d
BiplobKarmakar <52356149+BiplobKarmakar@users.noreply.github.com>
2023-06-04 22:35:55 +0600

    Update homeworkvehicle.py

--- homeworkvehicle.py
+++ homeworkvehicle.py
@@ -1,22 +1,11 @@
 '''1. Create a child class Bus that will inherit all of the variables and methods of the Vehicle class.
 Expected Output: Vehicle Name: School Volvo Speed: 180 Mileage: 12
 
-2. Create a Bus child class that inherits from the Vehicle class. The default fare charge of any vehicle is 
-seating capacity * 100. If Vehicle is Bus instance, we need to add an extra 10% on full fare as a 
-maintenance charge. So total fare for bus instance will become the final amount = total fare + 10% of 
-the total fare.
 '''
 
 class Vehicle:
        
-       '''
-        def __init__(self, name,speed,mileage):
-         
-         self._name = name
-         self._speed=speed
-         self._mileage=mileage
-                '''
-
+     
        def get_name(self):
           return self._name
       
@@ -43,14 +32,7 @@ class Vehicle:
 class Bus(Vehicle):
    pass
    
-  # def __init__(self,name,speed,mileage):
-       #super().__init__
-
-       
-      
-       
-    
-
+ 
 bus=Bus()
 bus.set_name('school volvo')
 bus.set_speed(180)
